06559c208f1ba714677fdf00f46e3eec30838aa7
Nikhil Bhintade <nikhil.bhintade0104@gmail.com>
2020-11-20 17:37:35 +0530

    basic graphql api

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
-const express = require("express");
+const express = require('express');
 const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res, next) => {
-    res.send("hello world, this is Nikhil");
-})
+app.use('/graphql', graphqlHTTP({
+    schema: buildSchema(`
+        type RootQuery{
+            events: [String!]
+        }
+
+        type RootMutation{
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Cooking', 'Selling', 'Coding']
+        }, //resolvers & commands should have same name
+        createEvent: (args) => {
+            const eventName = args.name;
+            return eventName;
+        }
+    },
+    graphiql: true
+}))
 
 app.listen(3000);
